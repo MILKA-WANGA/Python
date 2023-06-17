5d5624877e9fd5c30dc6e26a0335ac38163e97e5
Milka Wanga <bakhuyamilka@gmail.com>
2023-06-17 10:33:17 +0300

    Printed a type of variable

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ print(17,6,2023,sep='/')
 #Variables
 Num=17
 print(Num)
+print(type(Num))
+
 name="Joyce"
 print(name)
+print(type(name))
+
 a=1.23
 print(a)
+print(type(a))
+
 b=12.457889
 print(b)
+print(type(b))
